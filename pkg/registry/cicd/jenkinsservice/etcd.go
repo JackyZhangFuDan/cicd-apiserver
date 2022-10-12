9cb72602b1279f6525cdc379e61aa3d611d7ccfb
Jacky Zhang <jacky01.zhang@outlook.com>
2022-10-12 20:40:39 +0800

    enhance etcd

--- pkg/registry/cicd/jenkinsservice/etcd.go
+++ pkg/registry/cicd/jenkinsservice/etcd.go
@@ -1,19 +1,19 @@
-package cicd
+package jenkinsservice
 
 import (
 	"cicd-apiserver/pkg/apis/cicd"
 	"cicd-apiserver/pkg/registry"
 
-	gRegistry "k8s.io/apimachinery/pkg/registry/generic/registry"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apiserver/pkg/registry/generic"
+	gRegistry "k8s.io/apiserver/pkg/registry/generic/registry"
 	"k8s.io/apiserver/pkg/registry/rest"
 )
 
 func NewREST(scheme *runtime.Scheme, optsGetter generic.RESTOptionsGetter) (*registry.REST, error) {
 	strategy := NewStrategy(scheme)
 
-	store := gRegistry.Store{
+	store := &gRegistry.Store{
 		NewFunc:                  func() runtime.Object { return &cicd.JenkinsService{} },
 		NewListFunc:              func() runtime.Object { return &cicd.JenkinsServiceList{} },
 		PredicateFunc:            MatchJenkinsService,
---
+++ pkg/registry/cicd/jenkinsservice/strategy.go
@@ -0,0 +1,76 @@
+package jenkinsservice
+
+import (
+	"cicd-apiserver/pkg/apis/cicd"
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/apiserver/pkg/registry/generic"
+	"k8s.io/apiserver/pkg/storage"
+	"k8s.io/apiserver/pkg/storage/names"
+)
+
+type jenkinsServiceStrategy struct {
+	runtime.ObjectTyper
+	names.NameGenerator
+}
+
+func NewStrategy(typer runtime.ObjectTyper) jenkinsServiceStrategy {
+	return jenkinsServiceStrategy{typer, names.SimpleNameGenerator}
+}
+
+func GetAttrs(obj runtime.Object) (labels.Set, fields.Set, error) {
+	object, ok := obj.(*cicd.JenkinsService)
+	if !ok {
+		return nil, nil, fmt.Errorf("the object isn't a JenkinsService")
+	}
+	fs := generic.ObjectMetaFieldsSet(&object.ObjectMeta, true)
+	return labels.Set(object.ObjectMeta.Labels), fs, nil
+}
+
+func MatchJenkinsService(label labels.Selector, field fields.Selector) storage.SelectionPredicate {
+	return storage.SelectionPredicate{
+		Label:    label,
+		Field:    field,
+		GetAttrs: GetAttrs,
+	}
+}
+
+// CreateStrategy接口定义的方法
+func (jenkinsServiceStrategy) AllowCreateOnUpdate() bool {
+	return false
+}
+func (jenkinsServiceStrategy) Canonicalize(obj runtime.Object) {
+
+}
+func (jenkinsServiceStrategy) NamespaceScoped() bool {
+	return true
+}
+func (jenkinsServiceStrategy) PrepareForCreate(ctx context.Context, obj runtime.Object) {
+
+}
+func (jenkinsServiceStrategy) Validate(ctx context.Context, obj runtime.Object) field.ErrorList {
+	js := obj.(*cicd.JenkinsService)
+	return nil
+}
+func (jenkinsServiceStrategy) WarningsOnCreate(ctx context.Context, obj runtime.Object) []string {
+	return []string{}
+}
+
+// UpdateStrategy接口定义的方法
+func (jenkinsServiceStrategy) AllowUnconditionalUpdate() bool {
+	return false
+}
+func (jenkinsServiceStrategy) PrepareForUpdate(ctx context.Context, obj runtime.Object, old runtime.Object) {
+
+}
+func (jenkinsServiceStrategy) ValidateUpdate(ctx context.Context, obj, old runtime.Object) field.ErrorList {
+	return field.ErrorList{}
+}
+func (jenkinsServiceStrategy) WarningsOnUpdate(ctx context.Context, obj runtime.Object, old runtime.Object) []string {
+	return []string{}
+}
