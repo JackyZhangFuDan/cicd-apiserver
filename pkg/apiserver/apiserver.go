23a52655fab7ab35b5838892351df1ef94cf8744
Jacky <jacky01.zhang@outlook.com>
2023-10-15 08:34:05 +0800

    phase 8 - add controller

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -2,16 +2,26 @@ package apiserver
 
 import (
 	"cicd-apiserver/pkg/apis/cicd"
+	"cicd-apiserver/pkg/controller/jenkinsservice"
+	jsclient "cicd-apiserver/pkg/generated/clientset/versioned"
+	informerfactory "cicd-apiserver/pkg/generated/informers/externalversions"
 	cicdregistry "cicd-apiserver/pkg/registry"
 	jsstorage "cicd-apiserver/pkg/registry/cicd/jenkinsservice"
+	"os"
+	"path/filepath"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/apimachinery/pkg/version"
 	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
+	"k8s.io/client-go/kubernetes"
+	clientgorest "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
 
 	"cicd-apiserver/pkg/apis/cicd/install"
 )
@@ -36,7 +46,7 @@ func init() {
 	)
 }
 
-//如下环节制作Server的Config
+// 如下环节制作Server的Config
 type Config struct {
 	GenericConfig *genericapiserver.RecommendedConfig
 	// ExtraConfig   ExtraConfig // 如果有自己需要的config的话，可以扩展field
@@ -46,7 +56,7 @@ type completedConfig struct {
 	GenericConfig genericapiserver.CompletedConfig
 }
 
-//完善后的config
+// 完善后的config
 type CompletedConfig struct {
 	*completedConfig
 }
@@ -55,7 +65,7 @@ type CicdServer struct {
 	GenericAPIServer *genericapiserver.GenericAPIServer
 }
 
-//完善初始的config
+// 完善初始的config
 func (cfg *Config) Complete() CompletedConfig {
 	cconfig := completedConfig{
 		cfg.GenericConfig.Complete(),
@@ -67,7 +77,7 @@ func (cfg *Config) Complete() CompletedConfig {
 	return CompletedConfig{&cconfig}
 }
 
-//有了这个方法，完善后的config就可以制作server的instance了
+// 有了这个方法，完善后的config就可以制作server的instance了
 func (ccfg completedConfig) NewServer() (*CicdServer, error) {
 	genericServer, err := ccfg.GenericConfig.New("cicd-apiserver", genericapiserver.NewEmptyDelegate())
 	if err != nil {
@@ -92,6 +102,38 @@ func (ccfg completedConfig) NewServer() (*CicdServer, error) {
 	if err := server.GenericAPIServer.InstallAPIGroup(&apiGroupInfo); err != nil {
 		return nil, err
 	}
+
+	// 创建相关控制器
+	config, err := clientgorest.InClusterConfig()
+	if err != nil {
+		// fallback to kubeconfig
+		kubeconfig := filepath.Join("~", ".kube", "config")
+		if envvar := os.Getenv("KUBECONFIG"); len(envvar) > 0 {
+			kubeconfig = envvar
+		}
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			klog.ErrorS(err, "The kubeconfig cannot be loaded: %v\n")
+			panic(err)
+		}
+	}
+	coreAPIClientset, err := kubernetes.NewForConfig(config)
+
+	client, err := jsclient.NewForConfig(genericServer.LoopbackClientConfig)
+	if err != nil {
+		klog.Error("Can't create client set for CICD API Server during creating server")
+	}
+	jsInformerFactory := informerfactory.NewSharedInformerFactory(client, 0)
+	controller := jenkinsservice.NewJenkinsServiceController(jsInformerFactory.Autobusi().V1alpha().JenkinsServices(), coreAPIClientset)
+
+	// 向Server启动钩子中注入控制器启动函数
+	genericServer.AddPostStartHookOrDie("cicd-apiserver-controller", func(ctx genericapiserver.PostStartHookContext) error {
+		ctxjs, _ := wait.ContextForChannel(ctx.StopCh)
+		go func() {
+			controller.Run(ctxjs, 2)
+		}()
+		return nil
+	})
 
 	return server, nil
 }
---
+++ pkg/controller/jenkinsservice/jenkinsservice_controller.go
@@ -0,0 +1,168 @@
+package jenkinsservice
+
+import (
+	"cicd-apiserver/pkg/apis/cicd/v1alpha"
+
+	informer "cicd-apiserver/pkg/generated/informers/externalversions/cicd/v1alpha"
+	lister "cicd-apiserver/pkg/generated/listers/cicd/v1alpha"
+	"context"
+	"fmt"
+	"time"
+
+	apps "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	"k8s.io/apimachinery/pkg/util/wait"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+)
+
+type Controller struct {
+	jenkinsServiceSynced cache.InformerSynced
+	coreAPIClient        clientset.Interface
+	jenkinsServiceLister lister.JenkinsServiceLister
+	queue                workqueue.RateLimitingInterface
+
+	syncHandler func(ctx context.Context, key string) error
+}
+
+func NewJenkinsServiceController(jsInformer informer.JenkinsServiceInformer, coreAPIClient clientset.Interface) *Controller {
+	c := &Controller{
+		coreAPIClient:        coreAPIClient,
+		jenkinsServiceLister: jsInformer.Lister(),
+		jenkinsServiceSynced: jsInformer.Informer().HasSynced,
+
+		queue: workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "jenkinsservice"),
+	}
+	c.syncHandler = c.sync
+
+	jsInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		// 只跟踪JS的创建
+		AddFunc: func(obj interface{}) {
+			klog.Info("New Jenkins Service Object is found")
+			cast := obj.(*v1alpha.JenkinsService)
+			key, err := cache.MetaNamespaceKeyFunc(cast)
+			if err != nil {
+				klog.ErrorS(err, "Failed when extracting key for Jenkins Service Object")
+				return
+			}
+			c.queue.Add(key)
+		},
+	})
+	return c
+}
+
+// 启动控制器，直到 stopCh关闭
+func (c *Controller) Run(ctx context.Context, workers int) {
+	defer utilruntime.HandleCrash()
+	defer c.queue.ShutDown()
+
+	klog.Info("Starting Jenkins Service Controller")
+	defer klog.Info("Shutting down Jenkins Service Controller")
+
+	// 等待本地JS的cache同步完毕
+	klog.Info("Waiting for caches to sync for Jenkins Service controller")
+	if !cache.WaitForCacheSync(ctx.Done(), c.jenkinsServiceSynced) {
+		utilruntime.HandleError(fmt.Errorf("Unable to sync caches for Jenkins Service controller"))
+		return
+	}
+	klog.Infof("Caches are synced for Jenkins Service controller")
+
+	// 启动指定数量的协程，每个都跑控制循环
+	// wait.Until的作用是如果runWorker处理失败退出，那么再次启动它
+	for i := 0; i < workers; i++ {
+		go wait.UntilWithContext(ctx, c.runWorker, time.Second)
+	}
+
+	<-ctx.Done()
+}
+
+func (c *Controller) runWorker(ctx context.Context) {
+	for c.processNextWorkItem(ctx) {
+	}
+}
+
+func (c *Controller) processNextWorkItem(ctx context.Context) bool {
+	key, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	//告诉queue，这个key的事情你已经处理完毕了
+	defer c.queue.Done(key)
+
+	err := c.syncHandler(ctx, key.(string))
+	if err == nil {
+		//处理完毕没出错，告诉queue清理这个key相关的追踪记录，例如失败计数等
+		c.queue.Forget(key)
+		return true
+	}
+	//上报错误，这个调用允许插拔错误处理逻辑，例如集群监控
+	utilruntime.HandleError(fmt.Errorf("%v failed with: %v", key, err))
+	//把失败的key放入队列，但在最后，从而赢得一段冷静事件，马上处理的话可能还会失败
+	c.queue.AddRateLimited(key)
+	return true
+}
+
+// 一个控制循环的处理逻辑
+func (c *Controller) sync(ctx context.Context, key string) (err error) {
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		klog.ErrorS(err, "Failed to split meta namespace cache key", "cacheKey", name)
+	}
+
+	js, err := c.jenkinsServiceLister.JenkinsServices(namespace).Get(name)
+	if errors.IsNotFound(err) {
+		klog.Infof("Jenkins Service %s has been deleted", key)
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	var replicas int32
+	replicas = int32(js.Spec.InstanceAmount)
+	var selector map[string]string
+	selector["type"] = "jenkinsservice"
+	selector["jsname"] = name
+
+	// 创建一个Deployment
+	d := apps.Deployment{
+		TypeMeta: metav1.TypeMeta{APIVersion: "apps/v1", Kind: "Deployment"},
+		ObjectMeta: metav1.ObjectMeta{
+			UID:         uuid.NewUUID(),
+			Name:        name,
+			Namespace:   namespace,
+			Annotations: make(map[string]string),
+		},
+		Spec: apps.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{MatchLabels: selector},
+			Template: v1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: selector,
+				},
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Image:           "jenkins/jenkins:lts-jdk17",
+							ImagePullPolicy: "Always",
+						},
+					},
+				},
+			},
+		},
+	}
+	_, err = c.coreAPIClient.AppsV1().Deployments(namespace).Create(ctx, &d, metav1.CreateOptions{})
+	if err != nil {
+		klog.ErrorS(err, "Failed when creating deployment for Jenkins Service")
+		return err
+	}
+
+	klog.Infof("Successfully create deploymnet for Jenkins Service %s", key)
+	return nil
+}
